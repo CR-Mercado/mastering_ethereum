0725d72b3c024e364076393ab90fa96665d42663
Carlos R. Mercado <35497842+CR-Mercado@users.noreply.github.com>
2021-06-20 09:10:42 -0400

    Completed Challenges!
    Lotto Contract is next

--- remixbackup/.workspaces/default_workspace/contracts/27_Challenge7.sol
+++ remixbackup/.workspaces/default_workspace/contracts/27_Challenge7.sol
@@ -14,3 +14,30 @@ Deploy and test the contract on Rinkeby Testnet.
 
 */
 
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Game{
+    
+    address[] public players;
+    
+    function start() public { 
+     address _newplayer = msg.sender; // not sure what best practice is, here I store the msg sender before I push
+     players.push(_newplayer);   
+    }
+}
+
+/*
+Recommended Solution: 
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Game{
+    address[] public players;
+    
+    function start() public{
+        players.push(msg.sender); // directly adds msg sender 
+    }
+}
+
+*/
--- remixbackup/.workspaces/default_workspace/contracts/28_Challenge8.sol
+++ remixbackup/.workspaces/default_workspace/contracts/28_Challenge8.sol
@@ -8,13 +8,49 @@ pragma solidity >=0.5.0 <0.9.0;
 contract A{
     int public x = 10;
 
-    function f3() private view returns(int){
+    function f() private view returns(int){
+        return x;
+    }
+}
+
+Change the visibility specifier of f() so that it can be called from derived contracts as well. Do not set it as being public.
+Create a new contract that derives from A and call f() from the new contract.
+*/
+
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract A { 
+ int public x = 10; 
+ 
+ function f() internal view returns(int){  // internal allows derived contracts 
+  return x;   
+ }
+    
+}
+
+// derived contract 
+contract B is A { 
+    function getF() public view returns(int){ 
+     return f();   
+    }
+} 
+
+/*
+Recommended Solution: 
+pragma solidity >=0.5.0 <0.9.0;
+ 
+contract A{
+    int public x = 10;
+
+    function f() internal view returns(int){
         return x;
     }
     
 }
 
-Change the visibility specifier of f() so that it can be called from derived contracts as well. Do not set it as being public.
-
-Create a new contract that derives from A and call f() from the new contract.
+contract B is A{
+    int public xx = f(); // uses implicit getter from public variables 
+}
 */
